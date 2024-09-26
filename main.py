38f01b87cfcc92f58efd5adcbade5c6a386e9741
😈Sviatoslav🤑 <132163617+sviatoslav-shubnikov@users.noreply.github.com>
2024-09-26 12:12:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import math
 
-def calculate_h(x,y,z):
-    
-    h = ((x**(y+1) + math.exp(y-1)) * (1+abs(y-x)))/(1+x*abs(y-math.tan(z))) + ((abs(y-x)**2)/2) + ((abs(y-x)**3)/3)
-
-    return h
-
 x = float(input("Введите x: "))
 y = float(input("Введите y: "))
 z = float(input("Введите z: "))
 
-result = calculate_h(x,y,z)
-print(f"h = {result}")
+h = ((x**(y+1) + math.exp(y-1)) * (1+abs(y-x)))/(1+x*abs(y-math.tan(z))) + ((abs(y-x)**2)/2) + ((abs(y-x)**3)/3)
+
+print(f"h = {h}")
